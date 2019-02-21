94e9946ce3a707a30012af084f88c6a711e732b7
Richard van Heest <rvanheest@users.noreply.github.com>
2019-02-21 15:58:59 +0100

    Update src/main/scala/nl.knaw.dans.easy.bagstore/server/DefaultServletComponent.scala
    Co-Authored-By: stephanHogenboom <24325101+stephanHogenboom@users.noreply.github.com>

--- src/main/scala/nl.knaw.dans.easy.bagstore/server/DefaultServletComponent.scala
+++ src/main/scala/nl.knaw.dans.easy.bagstore/server/DefaultServletComponent.scala
@@ -23,7 +23,7 @@ import nl.knaw.dans.lib.logging.servlet._
 import nl.knaw.dans.lib.logging.servlet.masked.MaskedAuthorizationHeader
 import org.scalatra.{ Ok, ScalatraServlet }
 
-trait DefaultServletComponent extends DebugEnhancedLogging {
+trait DefaultServletComponent {
   this: BagStoresComponent =>
 
   val defaultServlet: DefaultServlet
