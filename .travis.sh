1ce1644f8957448901568db85eb0618ba1d2c053
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2020-02-27 22:57:01 +0100

    Removing apparently redundant pip3 install call

--- .travis.sh
+++ .travis.sh
@@ -24,10 +24,6 @@ GH_REPO=$(basename $TRAVIS_BUILD_DIR)
 REMOTE="https://${GH_TOKEN}@github.com/${GH_ORG}/${GH_REPO}"
 git remote set-url origin ${REMOTE}
 
-echo "START installing required Python packages..."
-pip3 install -r requirements.txt
-echo "DONE installing required Python packages."
-
 echo "START installing DANS mkdocs theme..."
 git clone https://github.com/Dans-labs/mkdocs-dans $HOME/mkdocs-dans
 pushd $HOME/mkdocs-dans
