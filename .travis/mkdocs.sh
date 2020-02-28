e529ea2eeadb4726eee34b658d85a34f24f99f55
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2020-02-28 17:31:44 +0100

    Test new travis config

--- .travis/mkdocs.sh
+++ .travis/mkdocs.sh
@@ -24,7 +24,7 @@ GH_REPO=$(basename $TRAVIS_BUILD_DIR)
 REMOTE="https://${GH_TOKEN}@github.com/${GH_ORG}/${GH_REPO}"
 git remote set-url origin ${REMOTE}
 
-pip install -r requiments.txt
+pip install -r .travis/requiments.txt
 
 echo "START installing DANS mkdocs theme..."
 git clone https://github.com/Dans-labs/mkdocs-dans $HOME/mkdocs-dans
